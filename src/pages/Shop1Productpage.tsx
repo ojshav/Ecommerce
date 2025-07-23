42ec0ffa12da2df7c1259c18ac58eac1f0b457db
Rishabh <awasthirishabh56@gmail.com>
2025-07-23 17:00:21 +0530

    merge two or more files in single

--- src/pages/Shop1Productpage.tsx
+++ src/pages/Shop1Productpage.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import '../styles/globals.css';
 import Header from '../components/shop/shop1/Header';
-import Hero from '../components/shop/shop1/Productpage/Hero';
+
 import FashionCardsSection from '../components/shop/shop1/Productpage/FashionCardsSection';
-import RatingsReviews from '../components/shop/shop1/Productpage/Rating';
+
 import SimilarProducts from '../components/shop/shop1/Productpage/SimilarProducts';
 import InstagramPromo from '../components/shop/shop1/Productpage/InstagramPromo';
 
@@ -19,9 +19,9 @@ function Shop1ProductPage() {
     <div className="min-h-screen bg-white scroll-smooth overflow-x-hidden">
       <Header />
       
-        <Hero />
+        
         <FashionCardsSection />
-        <RatingsReviews />
+        
         <SimilarProducts />
         <InstagramPromo />
     </div>  
