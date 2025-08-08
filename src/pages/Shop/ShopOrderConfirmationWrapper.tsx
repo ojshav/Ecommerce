a5348f3901a199d4e1b932aebcd7730280a3d610
Dev07-Harsh <150646921+Dev07-Harsh@users.noreply.github.com>
2025-08-08 17:05:30 +0530

    feat: Add order confirmation pages for multiple shops

---
+++ src/pages/Shop/ShopOrderConfirmationWrapper.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ShopOrderConfirmation from './ShopOrderConfirmation';
+
+export const Shop1OrderConfirmation: React.FC = () => (
+  <ShopOrderConfirmation shopId={1} shopName="Shop 1" />
+);
+
+export const Shop2OrderConfirmation: React.FC = () => (
+  <ShopOrderConfirmation shopId={2} shopName="Shop 2" />
+);
+
+export const Shop3OrderConfirmation: React.FC = () => (
+  <ShopOrderConfirmation shopId={3} shopName="Shop 3" />
+);
+
+export const Shop4OrderConfirmation: React.FC = () => (
+  <ShopOrderConfirmation shopId={4} shopName="Shop 4" />
+);
